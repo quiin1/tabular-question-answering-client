dfad2ea1ea10a0706ee150cce139a3540c5ca87d
quynhnt <trangquynh17@gmail.com>
2023-04-25 00:26:44 +0700

    merge

--- src/components/TableDisplay.jsx
+++ src/components/TableDisplay.jsx
@@ -1,18 +1,18 @@
 import {Form, InputGroup} from 'react-bootstrap'
 import '../styles/table.css'
 
-async function load(link) {
+let link = 'https://raw.githubusercontent.com/Yale-LILY/FeTaQA/main/end2end/data/fetaQA-v1_dev.json'
+
+async function randomTableArray(link) {
     let conn = await fetch(link);
     let data = await conn.json();
     let index = Math.round(Math.random() * data.data.length);
     return data.data[index].table_array;
 }
 
-
 export default function TableDisplay({table, setTable}) {
-    function loadLink(e) {
-        let link = 'https://raw.githubusercontent.com/Yale-LILY/FeTaQA/main/end2end/data/fetaQA-v1_dev.json'
-        load(link).then((tableArray) => {
+    function loadTable() {
+        randomTableArray(link).then((tableArray) => {
             setTable(tableArray)
         })
     }
@@ -26,13 +26,13 @@ export default function TableDisplay({table, setTable}) {
                     id="basic-url" 
                     aria-describedby="basic-addon3" 
                     readOnly
-                    placeholder="https://raw.githubusercontent.com/Yale-LILY/FeTaQA/main/end2end/data/fetaQA-v1_dev.json"
+                    placeholder={link}
                     />
                 <button 
                     id="urlBtn" 
                     className="btn btn-outline-secondary" 
                     type="button"
-                    onClick={e => loadLink(e)}
+                    onClick={loadTable()}
                 >
                     random
                 </button>
