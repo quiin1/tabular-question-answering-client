2f14a978a189161329b50849f45396d369e4cc79
quynhnt <trangquynh17@gmail.com>
2023-04-25 11:26:15 +0700

    delete MainHeader

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from 'react-dom';
 import "./styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MainHeader from "./components/MainHeader";
 import WidgetGrid from "./components/WidgetGrid";
 
 function MyApp() {
@@ -14,7 +13,6 @@ function MyApp() {
         content="upgrade-insecure-requests"
       />
       <div className="flex flex-col overflow-hidden">
-        {/* <MainHeader /> */}
         <div className="flex-1 text-center mb-4">
           <h1>Tapex Chatbot</h1>
         </div>
